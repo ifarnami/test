9357881b725ffa91d8b249c5ed5e9be9608f48d9
ifarnami <farnam.faryar@gmail.com>
2024-07-23 11:41:14 +0200

    test

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/": "https://ecommerce-api.liara.run",
+      "/uploads/": "https://ecommerce-api.liara.run",
+    },
+  },
+  build: {
+    outDir: "./build",
+  },
+});
